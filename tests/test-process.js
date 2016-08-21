2be5916de066ca355c37ad1af829370010393947
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-08-21 16:19:15 +0545

    function which processes the log is created and it's structure is defined

---
+++ tests/test-process.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var LOG_FILE_NAME = "example.log";
+var aggregatedLogResults = [];
+
+var aggregateLogs = function(){
+	var singleLine = [];
+
+	fs.readFile(LOG_FILE_NAME, "utf-8", function(error, data){
+		if (error) { 
+			console.log(error);
+			return;
+		}
+		var data = data.toString();
+		var linesOfLog = data.split('\n');
+
+		for (var lineCount = 0, length = linesOfLog.length; lineCount < length; lineCount++){
+			singleLineString = linesOfLog[lineCount].toString();
+			singleLine = convertToArray(singleLineString);
+			date = singleLine[0];
+
+			if(isNewDateLog(date)){
+				addDateLog(singleLine);
+			} else {
+				updateDateLog(singleLine, date);
+			}
+		}
+	});
+}
